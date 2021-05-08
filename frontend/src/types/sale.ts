517be796de9c0b1909653b7ef4782cafa41c6d97
Patrick Anjos <patrickpqdt87289@gmail.com>
2021-05-08 17:58:17 -0300

    BarChart integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,2 +1,2 @@
-export type SaleSum = {sellerName: string, sum: number}
-
+export type SaleSum = { sellerName: string; sum: number; }
+export type SaleSuccess = { sellerName: string; visited: number; deals: number; } 
